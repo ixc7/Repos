79c9edf096e2fb86ab22c41cc6c99092c4f6ea2f
ixc7 <isaacxpreston7@gmail.com>
2023-12-17 19:08:41 -0500

    file preview with scrollable list

--- previewFiles.sh
+++ previewFiles.sh
@@ -3,6 +3,8 @@
 # https://docs.github.com/en/rest/repos/contents
 # https://docs.github.com/en/rest/git/trees
 
+source "$(dirname ${0})/scrollableList.sh"
+
 repoName="ixc7/repos"
 branchName=$(gh api "repos/${repoName}" | jq -r '.default_branch') # "main"
 treeJSON=$(gh api "repos/${repoName}/git/trees/${branchName}?recursive=true")
@@ -10,21 +12,37 @@ treeJSON=$(gh api "repos/${repoName}/git/trees/${branchName}?recursive=true")
 declare -a pathNames="($(echo "${treeJSON}" | jq '.tree[] | if .type == "blob" then .path else empty end'))"
 declare -a urlNames="($(echo "${treeJSON}" | jq '.tree[] | if .type == "blob" then .url else empty end'))"
 
-ns
-echo "found ${#pathNames[@]} files in ${repoName} (${branchName})"
+# ns
+# echo "found ${#pathNames[@]} files in ${repoName} (${branchName})"
 
-for i in "${!pathNames[@]}"; do
-  echo "
-    filename: ${pathNames[i]}
-    url: ${urlNames[i]}
-  "
-  apiURL=$(echo "${urlNames[i]}" | sed 's/.*github.com\/*//')
-  gh api "${apiURL}" | jq -r '.content' | base64 -d | bat -pp -l "${pathNames[i]##*.}"
-  read
+# for i in "${!pathNames[@]}"; do
+#   echo "
+#     filename: ${pathNames[i]}
+#     url: ${urlNames[i]}
+#   "
+#   apiURL=$(echo "${urlNames[i]}" | sed 's/.*github.com\/*//')
+#   gh api "${apiURL}" | jq -r '.content' | base64 -d | bat -pp -l "${pathNames[i]##*.}"
+#   read
+# done
+
+tempfile=$(mktemp)
+
+while true; do
+  selection=""
+  apiURL=""
+
+  _scrollableList "${pathNames[@]}" -o "${tempfile}" &&
+    selection=$(cat "${tempfile}")
+
+  [[ "${#selection}" -eq 0 ]] && break
+  
+  for i in "${!pathNames[@]}"; do
+    if [[ "${pathNames[i]}" == "${selection}" ]]; then
+      apiURL=$(echo "${urlNames[i]}" | sed 's/.*github.com\/*//')
+      gh api "${apiURL}" | jq -r '.content' | base64 -d | bat -p # -l "${pathNames[i]##*.}"
+      break
+    fi
+  done
+  
 done
 
-# echo "${treeJSON}" |
-# jq -r '.tree[] | .url' |
-# sed 's/.*github.com\/*//' |
-# xargs -n 1 gh api |
-# jq -r '.content' | base64 -d |
